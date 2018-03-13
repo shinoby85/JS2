88170f4fed649c8694c5b61fd4d20227bfbdecb0
Shinoby85 <ponk@tut.by>
2018-03-14 00:22:00 +0300

    Добавление стилей

--- Lesson 1/styleSubMenu.js
+++ Lesson 1/styleSubMenu.js
@@ -18,10 +18,32 @@ function styleSubMenu() {
     nav_menu.style.textAlign='center';
     nav_menu.style.padding='10px 0';
     var nav_li_child=nav.querySelectorAll('.my-menu > li');
-    var nav_li_child_after=nav.getComputedStyle(nav_li_child,':after');
     for(i=0;i<nav_li_child.length;i++){
         nav_li_child[i].style.display='inline-block';
         nav_li_child[i].style.position='relative';
+    }
+    var nav_menu_li_a=nav.querySelectorAll('.my-menu > li > a');
+    for(i=0;i<nav_menu_li_a.length;i++){
+        nav_menu_li_a[i].style.padding='12px 26px';
+        nav_menu_li_a[i].style.color='#767676';
+        nav_menu_li_a[i].style.textTransform='uppercase';
+        nav_menu_li_a[i].style.fontWeight='bold';
+        nav_menu_li_a[i].style.letterSpacing='1px';
+        nav_menu_li_a[i].style.fontFamily='"Exo 2", sans-serif';
+    }
+    var nav_li_a=nav.getElementsByTagName('a');
+    for(i=0;i<nav_li_a.length;i++){
+        nav_li_a[i].onmouseover=function (e) {
+            nav_li_a[i].style.color='#767676';
+        };
+        
+
+        /*nav_li_a[i].onmouseover = function(){
+            nav_li_a[i].style.color='green';
+        }*/
+        /*nav_li_a[i].onmouseout = function(){
+            nav_li_a[i].style.color = '';
+        }*/
 
     }
 
