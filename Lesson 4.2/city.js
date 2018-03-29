7af7104a77ad2ca1250721e95c446acffb9b0533
Shinoby85 <ponk@tut.by>
2018-03-29 14:15:55 +0300

    2-3 задания

--- Lesson 4.2/city.js
+++ Lesson 4.2/city.js
@@ -15,6 +15,35 @@ $(function () {
            console.log(error);
        }
    });
+   $('#cityQuery').keyup(function () {
+       var calLetter=$(this).val().length;
+       if (calLetter>=3){
+           var flagAddCity=false;
+           var searchStr='<ul>';
+           var strCity=$(this).val();
+           for (var i=0; i<ajaxData.length;i++){
+               if(ajaxData[i][1].toLowerCase().indexOf(strCity)!=-1){
+                   searchStr+='<li>'+ajaxData[i][1]+'</li>';
+                   flagAddCity=true;
+               }
+           }
+           searchStr+='</ul>';
+           if (flagAddCity===true){
+               $('#cityAComplete').show();
+               $('#cityAComplete').html(searchStr);
+           }
+           $('#cityAComplete ul li').on('click',function (event) {
+               $('#cityAComplete').hide();
+               $('#cityQuery')[0].value=event.target.innerText;
+           })
+
+       }
+       else {
+           $('#cityAComplete').hide();
+           $('#cityAComplete').html('');
+       }
+   });
+
 
 
 
